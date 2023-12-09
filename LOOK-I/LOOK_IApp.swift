4fc359e6b509fa4c750f3d6c84a79a4cfd32b497
Yooshin Kim <yusinkim.or@gmail.com>
2023-12-09 19:44:03 +0900

    Initial Commit

---
+++ LOOK-I/LOOK_IApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOOK_IApp.swift
+//  LOOK-I
+//
+//  Created by 김유신 on 2023/12/09.
+//
+
+import SwiftUI
+
+@main
+struct LOOK_IApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
