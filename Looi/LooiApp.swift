66ddc839a36508f275b8a50cb5dd929abf5db605
Yooshin Kim <yusinkim.or@gmail.com>
2024-01-02 15:48:49 +0900

    김유신 : Firebase 연결 (라이브러리 설치 및 service.info 추가)

--- Looi/LooiApp.swift
+++ Looi/LooiApp.swift
@@ -4,15 +4,26 @@
 
 import SwiftUI
 import WebKit
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct LooiApp: App {
     @State private var isLoading = true
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var body: some Scene {
         WindowGroup {
             ZStack {
-                WebView{
+                WebView {
                     self.isLoading = false
                 }
                 
